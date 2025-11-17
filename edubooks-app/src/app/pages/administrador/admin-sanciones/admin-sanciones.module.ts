abb613c205b6d420be986e6e5414d7361bfb61f1
deiker12345 <Deikergomez999@gmail.com>
2025-11-17 15:19:38 -0500

    feat(admin-sanciones): add crear-sancion page with reactive forms
    Add new CrearSancionPage component and include ReactiveFormsModule to support form handling in the admin sanctions module

--- edubooks-app/src/app/pages/administrador/admin-sanciones/admin-sanciones.module.ts
+++ edubooks-app/src/app/pages/administrador/admin-sanciones/admin-sanciones.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { AdminSancionesPageRoutingModule } from './admin-sanciones-routing.module';
 import { AdminSancionesPage } from './admin-sanciones.page';
+import { CrearSancionPage } from './crear-sancion/crear-sancion.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     AdminSancionesPageRoutingModule
   ],
-  declarations: [AdminSancionesPage]
+  declarations: [AdminSancionesPage, CrearSancionPage]
 })
 export class AdminSancionesPageModule {}
